75a500ebe18bba8626eaa4e59a3a042586f48687
santiadsi <slopez865@misena.edu.co>
2015-05-12 11:45:19 -0500

    estableciendo conexion

--- src/java/conectadb/conectadb.java
+++ src/java/conectadb/conectadb.java
@@ -9,6 +9,56 @@ package conectadb;
  *
  * @author Andres
  */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class conectadb {
+    
+     /**
+     * nombre de la base de datos
+    */
+    public String db = "mascotas.mysql";
+    
+    /**
+     * URL del driver
+    */
+    public String url = "jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=convertToNull"+db;
+    
+    /**
+     * usuario de la bd
+    */
+    public String user = "root";
+    
+    
+    /**
+     * contraseña del usuario de la bd
+    */
+    public String pass = "";
+
+   /**
+    * 
+    * @return la coneccion a la base de datos
+    */
+    
+   public Connection Conectar(){
+
+       Connection link = null;
+
+       try{
+
+           Class.forName("org.sqlite.JDBC");
+
+           link = DriverManager.getConnection(this.url, this.user, this.pass);
+
+       }catch(Exception ex){
+
+           JOptionPane.showMessageDialog(null, ex);
+
+       }
+
+
+       return link;
+
+   }
     
 }
